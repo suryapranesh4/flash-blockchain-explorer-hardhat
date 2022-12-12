316936399499ba1cd8a0e7a24ed4f1ead3d43199
suryapranesh4 <suryapranesh@yahoo.ca>
2022-12-11 19:56:39 -0500

    initial route added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  console.log("App get /");
+  res.json({ hello: "world" });
+});
+
 module.exports = app;
